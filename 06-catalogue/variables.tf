567cb32a6d91690d17503070f1e4473f757240b3
Kalyanibazaru <bazaru.kalyani@gmail.com>
2024-03-05 16:40:37 +0530

    roboshop-infra-dev

---
+++ 06-catalogue/variables.tf
@@ -0,0 +1,23 @@
+variable "common_tags" {
+  default = {
+    Project     = "roboshop"
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+variable "tags" {
+    default = {
+        Component = "catalogue"
+    }
+}
+variable "project_name" {
+  default = "roboshop"
+}
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_name" {
+  default = "bkdevops.online"
+}
